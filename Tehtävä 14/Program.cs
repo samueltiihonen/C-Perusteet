ccb7d277b10f5ff5496ccbc501aafb015b3a79b5
samueltiihonen <samuel_t@windowslive.com>
2023-04-03 15:34:36 +0300

    Tehtävä 14 lisätty githubiin

---
+++ Tehtävä 14/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtävä_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine();
+            
+            int; numb;
+            Console.WriteLine("ilmoita onko käyttäjän luku parillinen vai pariton ");
+            Console.WriteLine("Enter any Number");
+            int; numb.Parse
+
+             if (numb % 2 == 0)
+            {
+                Console.WriteLine("Number is even")
+}
+            else
+            {
+                Console.WriteLine("Number is odd")
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+            Console.Readkey();
+    }
+}
