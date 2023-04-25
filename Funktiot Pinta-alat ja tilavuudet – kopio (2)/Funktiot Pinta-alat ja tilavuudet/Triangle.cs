31e4e3723987c37581181897575a1b076e64af6e
samueltiihonen <samuel_t@windowslive.com>
2023-04-25 19:53:53 +0300

    25.04.2023 Tehtävät

---
+++ Funktiot Pinta-alat ja tilavuudet – kopio (2)/Funktiot Pinta-alat ja tilavuudet/Triangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funktiot_Pinta_alat_ja_tilavuudet
+{
+    class Triangle
+    {
+    
+        // Properties
+        public double Height { get; set; }
+
+        public double Base { get; set; }
+        
+        // Costructors
+
+        public Triangle(double height, double baseParameter)
+        {
+            Height = height;
+            Base = baseParameter;
+        }
+
+        // Methods 
+        // Ei tarvitse static, koska tätä käytetään luokan objektista.
+        // Esim: Triangle test = new Triangle(2, 5); - Luodaan objekti luokasta, nimeltä "test"
+        // Console.WriteLine(test.Area()); - käytetään obkektin metodia.
+        public double Area()
+        {
+            double result = 0;
+
+            result = Base * Height / 2;
+
+            return result; 
+        }
+
+
+        public double Perimeter()
+        {
+            // c = () 
+
+            double result = 0;
+
+            result = Height + Base + Math.Sqrt(Math.Pow(Base, 2) + Math.Pow(Height, 2));
+
+            return result;
+        }
+
+
+    }
+}
