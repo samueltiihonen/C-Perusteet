d32d23a31588a5f4e6740afe0646d769eaf93be6
samueltiihonen <samuel_t@windowslive.com>
2023-05-12 19:54:19 +0300

    C# Kaikkien aiheiden kertaus tehtävä

---
+++ C sharp Kaikkien aiheiden kertaus Tehtävä/C sharp Kaikkien aiheiden kertaus Tehtävä/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_Kaikkien_aiheiden_kertaus_Tehtävä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // C# Number Guessing Game
+
+            Random random = GenerateRandomNumber(13);
+            int targetNumber = random.Next(1, 101);
+
+            Console.WriteLine("Arvaa luku 1 ja 100 väliltä: ");
+            bool guessCorrectly = false;
+
+            while(!guessedCorrectly)
+            {
+                string userInput = Console.ReadLine();
+                int userGuess;
+
+                if (int. TryParse(userInput, out userGuess))
+                
+            }
+         
+
+
+            
+            string userInput = Console.ReadLine();
+            int userGuess = int.Parse(userInput);
+
+            if (userGuess < targetNumber)
+            {
+                Console.WriteLine("Isompi!");
+            }
+            else if (userGuess > targetNumber)
+            {
+                Console.WriteLine("Pienempi!");
+            }
+            else
+            {
+                Console.WriteLine("Onneksi olkoon! Arvasit oikein!");
+            }
+            
+            
+            
+            
+            
+            
+            
+            Console.ReadKey();
+
+        }
+    }
+}
