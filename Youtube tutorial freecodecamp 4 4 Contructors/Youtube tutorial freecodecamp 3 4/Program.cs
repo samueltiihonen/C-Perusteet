790a2851ea2fa0873f15c5c384fff9b897ef8216
samueltiihonen <samuel_t@windowslive.com>
2023-05-13 01:10:06 +0300

    C# Youtube tutorial 4/4

---
+++ Youtube tutorial freecodecamp 4 4 Contructors/Youtube tutorial freecodecamp 3 4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Youtube_tutorial_freecodecamp_3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Constructions 
+
+            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+          
+
+            Book book2 = new Book("Lord of the Rings", "Tolkein", 700);
+
+
+            Console.WriteLine(book1.title);
+
+
+            Console.ReadLine();
+
+
+
+
+        }
+            
+            
+           
+
+
+
+
+
+         
+
+            
+
+
+
+
+
+
+     
+
+
+
+
+
+
+            
+            
+            
+        
+    }
+}
