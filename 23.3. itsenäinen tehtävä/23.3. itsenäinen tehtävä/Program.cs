242897dddf072ab7dd5369a7871a36b736a1ee3d
samueltiihonen <samuel_t@windowslive.com>
2023-08-21 17:37:31 +0300

    23.3 itsenäinen tehtävä. Pinta ala laskuri

---
+++ 23.3. itsenäinen tehtävä/23.3. itsenäinen tehtävä/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23._3.itsenäinen_tehtävä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä leveys (metreinä):");
+            double leveys = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Syötä korkeus (metreinä):");
+            double korkeus = Convert.ToDouble(Console.ReadLine());
+
+            double pintaAla = leveys * korkeus;
+
+            Console.WriteLine("Pinta-ala: " + pintaAla + " neliömetriä");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
